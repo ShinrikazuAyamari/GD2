7f184b2237cba8d316314a0210c6dfef09a5f3d4
kaan8015 <kalle.andersson.8015@student.uu.se>
2025-02-04 13:11:47 +0100

    Simple enemy ai
    The enemy detects the player and walks.
    
    The enemy will be able to see through walls and doesn't have pathfinding.

--- Assets/Scripts/Enemies/GenericEnemy.cs
+++ Assets/Scripts/Enemies/GenericEnemy.cs
@@ -5,6 +5,11 @@ public class GenericEnemy : MonoBehaviour
 
     public int health;
 
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+    public float sight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +20,17 @@ public class GenericEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (distance < sight)
+        {
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
     }
 
     public void onAttacked(int damage)
