3d6bd74b62d347acbb9828632fcdd6fb7f0b172d
kaan8015 <kalle.andersson.8015@student.uu.se>
2025-02-05 12:52:13 +0100

    Unfinished optimized GrabAndRelease
    This is unfinished but aims to optimize GrabObjectScript

--- Assets/Scripts/Player/GrabObjectScript.cs
+++ Assets/Scripts/Player/GrabObjectScript.cs
@@ -5,15 +5,24 @@ using UnityEngine.UIElements;
 public class GrabObjectScript : MonoBehaviour
 {
     public Transform grabDetect;
-    public Transform objectHolder;
     public float rayDist;
-    bool holding;
+
+    int mouseNumb;
 
     Vector2 mousePosition;
 
-    GameObject grabbedObject;
-    public float shootStrength;
+    //  Left hand stuff
+    public Transform objectHolderLeft;
+    bool holdingLeft;
+    GameObject grabbedObjectLeft;
 
+    //  Right hand stuff
+
+    public Transform objectHolderRight;
+    bool holdingRight;
+    GameObject grabbedObjectRight;
+
+    public float shootStrength;
     public float grabSpeed;
 
     // Update is called once per frame
@@ -21,32 +30,44 @@ public class GrabObjectScript : MonoBehaviour
     {
         mousePosition = mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-
         if (Input.GetMouseButtonDown(0))
         {
-            if(!holding)
-            {
-                RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, direction, rayDist);
-                if (grabCheck.collider != null && grabCheck.collider.tag == "Object")
-                {
-                    //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero; // Sets the grabbed object's velocity to zero so it won't move when grabbed. 
-                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().simulated = false; // Disable simulation so the objects hurt box won't be enabled while walking around with it.
-                    grabbedObject = grabCheck.collider.gameObject;
-                    holding = true;
-                }
+            GrabAndRelease(0, objectHolderLeft, ref holdingLeft, ref grabbedObjectLeft);
+        }
+    }
 
-            }
-            else
+
+    private void GrabAndRelease(int handNumb, Transform objectHolder, ref bool holding, ref GameObject grabbedObject)
+    {
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        // This code is gonna need:
+
+        //Grabbedobject (left or right)
+        //Holding
+        //Which input
+
+        if (!holding)
+        {
+            RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, direction, rayDist);
+            if (grabCheck.collider != null && grabCheck.collider.tag == "Object")
             {
-                grabbedObject.transform.parent = null;
-                //grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                grabbedObject.GetComponent<Rigidbody2D>().simulated = true;
-                grabbedObject.GetComponent<Rigidbody2D>().linearVelocity = transform.up * shootStrength;
-                grabbedObject = null;
-                holding = false;
+                //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                Rigidbody2D grabRigidbody = grabCheck.collider.gameObject.GetComponent<Rigidbody2D>();
+                grabRigidbody.linearVelocity = Vector2.zero; // Sets the grabbed object's velocity to zero so it won't move when grabbed. 
+                grabRigidbody.simulated = false;     // Disable simulation so the objects hurt box won't be enabled while walking around with it.
+                grabbedObject = grabCheck.collider.gameObject;
+                holding = true;
             }
+
+        }
+        else
+        {
+            grabbedObject.transform.parent = null;
+            //grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            grabbedObject.GetComponent<Rigidbody2D>().simulated = true;
+            grabbedObject.GetComponent<Rigidbody2D>().linearVelocity = transform.up * shootStrength;
+            grabbedObject = null;
+            holding = false;
         }
 
         if (grabbedObject != null)
