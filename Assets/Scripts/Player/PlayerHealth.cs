1797e28c445c5aebacc8a2df5adc5bde3b93318b
kaan8015 <kalle.andersson.8015@student.uu.se>
2025-02-12 05:10:30 +0100

    Slider reacts to health updates
    The slider now fills whenever the player takes damage

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,17 +1,26 @@
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-
+    public int maxHealth;
     public int health;
+    public Slider healthSlider;
+
+    private void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.minValue = 0;
+    }
 
     public void ReduceHealth(int increaseAmount)
     {
         health += increaseAmount;
+        healthSlider.value = health;
         
-        if (health >= 100)
+        if (health >= maxHealth)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name); // To always load the current scene when the player dies. 
         }
